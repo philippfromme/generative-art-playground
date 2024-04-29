f2c4041dc154145f2d218884e77e79ed081cd458
Philipp Fromme <philipp.fromme@camunda.com>
2024-04-29 17:47:40 +0200

    chore: allow directories

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const sketch = process.env.SKETCH || 'hello-world';
 
+// check if ./src/sketches/${sketch} is directory and if so, use index.js
+const fs = require('fs');
+
+let entry = `./src/sketches/${sketch}`;
+
+if (fs.existsSync(entry)) {
+  if (fs.lstatSync(entry).isDirectory()) {
+    entry = `./src/sketches/${sketch}/index.js`;
+  } else {
+    entry = `./src/sketches/${sketch}.js`;
+  }
+}
+
 module.exports = {
-  entry: `./src/sketches/${sketch}.js`,
+  entry,
   output: {
     path: __dirname + '/dist',
     filename: 'sketch.js'
