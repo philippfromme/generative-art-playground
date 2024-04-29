74c71f9aa849de8e778296ae69e97f7b8f400770
Philipp Fromme <philipp.fromme@camunda.com>
2024-04-29 17:42:43 +0200

    chore: add example script

--- src/sketches/hello-world.js
+++ src/sketches/hello-world.js
@@ -1,17 +1,27 @@
-import P5 from 'p5';
+import p5 from "p5";
 
-const sketch = (p) => {
-  p.setup = () => {
-    p.createCanvas(400, 400);
-  }
+new p5(sketch => {
+  sketch.setup = () => {
+    sketch.createCanvas(sketch.windowWidth, sketch.windowHeight, sketch.WEBGL);
+  };
 
-  p.draw = () => {
-    p.background(255);
+  sketch.draw = () => {
+    sketch.background(0);
 
-    p.fill(0);
+    sketch.translate(0, 0, 0); // origin is at center of sketch
+    sketch.normalMaterial();
+    sketch.push();
+    sketch.rotateZ(sketch.frameCount * 0.01);
+    sketch.rotateX(sketch.frameCount * 0.01);
+    sketch.rotateY(sketch.frameCount * 0.01);
+    sketch.torus(
+      Math.min(sketch.windowWidth, sketch.windowHeight) / 4,
+      Math.min(sketch.windowWidth, sketch.windowHeight) / 12
+    );
+    sketch.pop();
+  };
 
-    p.ellipse(200, 200, 100);
-  }
-}
-
-new P5(sketch);
+  sketch.windowResized = () => {
+    sketch.resizeCanvas(sketch.windowWidth, sketch.windowHeight);
+  };
+});
